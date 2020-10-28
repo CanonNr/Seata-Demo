1f0aac8ff6ad7bb8340dc12ba94facb8ce46e8a8
Canon <717437709@qq.com>
2020-10-28 19:28:30 +0800

    Seata AT模式

--- service-1/src/main/java/com/lksun/service1/controller/Controller.java
+++ service-1/src/main/java/com/lksun/service1/controller/Controller.java
@@ -2,9 +2,11 @@ package com.lksun.service1.controller;
 
 import com.lksun.service1.dao.OrderDao;
 import com.lksun.service1.entity.Order;
+import io.seata.spring.annotation.GlobalTransactional;
 import org.aspectj.weaver.ast.Or;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -15,17 +17,16 @@ public class Controller {
     OrderDao orderDao;
 
     @RequestMapping("/rm")
+    @GlobalTransactional(rollbackFor = Exception.class)
     public String action(){
-        try{
+
             Order order = new Order();
             order.setStatus(1);
             orderDao.insert(order);
 
             rm2();
             return "Success";
-        }catch (Exception e){
-            return "Fail";
-        }
+
 
     }
 
--- service-2/src/main/java/com/lksun/service2/controller/Controller.java
+++ service-2/src/main/java/com/lksun/service2/controller/Controller.java
@@ -1,6 +1,7 @@
 package com.lksun.service2.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.lksun.service2.dao.OrdersDao;
@@ -13,9 +14,11 @@ public class Controller {
     OrdersDao ordersDao;
 
     @GetMapping("/rm")
+    @Transactional
     public void rm(){
         Orders order = new Orders();
         order.setStatus(true);
+        Integer i = 1/1;
         ordersDao.insert(order);
     }
 }
