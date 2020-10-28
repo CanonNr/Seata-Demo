d75277037944c03095ba234d5290b54381211a64
Canon <717437709@qq.com>
2020-10-28 17:59:03 +0800

    service2

---
+++ service-2/src/main/java/service2/dao/OrdersDao.java
@@ -0,0 +1,17 @@
+package service2.dao;
+
+import service2.entity.Orders;
+
+public interface OrdersDao {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Orders record);
+
+    int insertSelective(Orders record);
+
+    Orders selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Orders record);
+
+    int updateByPrimaryKey(Orders record);
+}
